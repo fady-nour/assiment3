05924f7f2f1791e68425bf85e1238151c5c4b7c8
Fadynour <fadynour194@gmail.com>
2025-06-27 20:38:48 +0300

    Q10

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -83,7 +83,23 @@
             //           double bmi = weight / (height * height);
             //           Console.WriteLine($"Your BMI is: {bmi:F2}");
             #endregion
-         
+            #region Q10
+            //Console.Write("Enter the temperature: ");
+            //double temperature = Convert.ToDouble(Console.ReadLine());
+            //if (temperature < 10)
+            //{
+            //    Console.WriteLine("Just Cold");
+            //}
+            //else if (temperature > 30)
+            //{
+            //    Console.WriteLine("Just Cold");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Just Good");
+            //}
+            #endregion
         }
     }
 }
