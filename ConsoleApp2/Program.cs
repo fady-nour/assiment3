3b72ca70b86b26177e022cdcaa178c400aa1f652
Fadynour <fadynour194@gmail.com>
2025-06-27 20:43:51 +0300

    Q17

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -181,6 +181,30 @@
             // }
 
             #endregion
+            #region  Q17
+
+
+            //Console.Write("Enter month number (1-12): ");
+            //int month = Convert.ToInt32(Console.ReadLine());
+
+            //if (month == 1  month == 3  month == 5  month == 7  month == 8  month == 10  month == 12)
+            //{
+            //    Console.WriteLine("Days in Month: 31");
+            //}
+            //else if (month == 4  month == 6  month == 9 || month == 11)
+            //{
+            //    Console.WriteLine("Days in Month: 30");
+            //}
+            //else if (month == 2)
+            //{
+            //    Console.WriteLine("Days in Month: 28 or 29 (leap year)");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid month number");
+            //}
+
+            #endregion
         }
     }
 }
