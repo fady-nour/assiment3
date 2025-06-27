c25a89ae6a9f6294dffa478dc24f50a90d1bdc45
Fadynour <fadynour194@gmail.com>
2025-06-27 20:57:46 +0300

    Q1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,13 +10,8 @@
             ////int number = Convert.ToInt32(Console.ReadLine());
             //Console.WriteLine(number);
             #endregion
-            #region Q2
-            //Console.WriteLine("Enter number");
+           
 
-            //int number2 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(number2);
-            //  System.FormatException: 'The input string 'hjjkljlkj' was not in a correct format.'
-            #endregion
         }
     }
 }
